e94058b56e6d81933dbcb22f4bbd96c5ea0886ff
Dominik-Slawkowski <dominik.slawkowski@gmail.com>
2017-11-27 11:10:25 +0000

    Enemy.cs edited online with Bitbucket Uzupełnienie brakow z zadania domowego z lab 2

--- Lab 2/DominikSlawkowskiLab2 - zadanie domowe/DominikSlawkowskiLab2/Enemy.cs
+++ Lab 2/DominikSlawkowskiLab2 - zadanie domowe/DominikSlawkowskiLab2/Enemy.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +8,25 @@ namespace DominikSlawkowskiLab2
 {
     abstract class Enemy
     {
+        /// <summary>
+        /// Wartości startowe dla każdego przewciwnika.
+        /// </summary>
         protected int Health = 100;
         protected int Fear = 30;
 
+        /// <summary>
+        /// Metoda zwracająca aktualne zdrowie przeciwnika.
+        /// </summary>
+        /// <returns></returns>
         public int GetHealth()
         {
             return Health;
         }
 
+        /// <summary>
+        /// Metoda zmniejszająca wartość życia postaci po otrzymaniu obrażeń.
+        /// </summary>
+        /// <param name="damage"></param>
         public void TakeDamage(int damage)
         {
             Health -= damage;
