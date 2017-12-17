072be7ebbae7c172c393fac05f80d09632b0f500
Dominik-Slawkowski <domino39@wp.pl>
2017-12-18 00:38:28 +0100

    Zadanie domowe lab 6

---
+++ Lab 6/DominikSlawkowskiLab6 - zadanie domowe/DominikSlawkowskiLab5/Dtos/PostDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DominikSlawkowskiLab5.Dtos
+{
+    public class PostDto
+    {
+        public int Id { get; set; }
+        [Required] //wymagane pole przy dodawaniu
+        public string Title { get; set; }
+        [Required]
+        public string Body { get; set; }
+        public string Author { get; set; }
+    }
+}
